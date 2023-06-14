189e4493ea32621f0ba2fa7998987b294f1f6007
loicy <36493474+loicy@users.noreply.github.com>
2023-06-14 17:51:40 +0200

    Update config
    Update config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-/** @type {import('next').NextConfig} */
+const nextTranslate = require('next-translate-plugin')
+
 const nextConfig = {
+  experimental: {
+    runtime: 'experimental-edge',
+  },
+  swcMinify: true,
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  ...nextTranslate(),
+  ...nextConfig,
+}
