d2fa26875c09c17d126563d1073be348a7e23ac8
loicy <36493474+loicy@users.noreply.github.com>
2023-06-14 21:54:22 +0200

    update next translate config for edge
    update next translate config for edge

--- next.config.js
+++ next.config.js
@@ -1,14 +1,3 @@
 const nextTranslate = require('next-translate-plugin')
 
-const nextConfig = {
-  experimental: {
-    runtime: 'experimental-edge',
-  },
-  swcMinify: true,
-  reactStrictMode: true,
-}
-
-module.exports = {
-  ...nextTranslate(),
-  ...nextConfig,
-}
+module.exports = nextTranslate()
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,7 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import useTranslation from 'next-translate/useTranslation'
 
 export const runtime = 'experimental-edge';
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home(audiovisuals) {
   const { t } = useTranslation("home")
